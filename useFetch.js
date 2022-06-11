e673dcd1b03687f19300d23f2a59cc451c78d56e
Andres Rocha <juanrocha.exe@gmail.com>
2022-06-11 06:48:31 +0100

    repo hooks

---
+++ useFetch.js
@@ -0,0 +1,37 @@
+import { useEffect, useRef, useState } from "react"
+
+
+export const useFetch = ( url ) => {
+
+    const [state, setState] = useState({data: null, loading: true, error: null})
+    const isMounted = useRef(true);
+
+    useEffect(() => {
+    
+      return () => {
+        isMounted.current = false;
+      }
+    }, [])
+    
+
+    useEffect(() => {
+        
+        fetch(url)
+        .then(resp => resp.json() )
+        .then( data =>{
+
+            if(isMounted.current){
+                setState({
+
+                    data, 
+                    loading: false,
+                    error: null
+                })
+            }else console.log("setState no se llamo")
+            
+        })
+
+    }, [url])
+    
+    return state;
+}
